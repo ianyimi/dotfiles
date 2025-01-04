c09232f27e0d69f366f01776cf2096a9f267aef3
yonko-zaye <isaiah@edaptschools.com>
2025-01-04 02:34:28 -0800

    feat: update sketchybar so it works on my new work macbook, todo; update sketchybar with always updating aerospace bash script to always get most up to date aerospace window config with currnt non-empty windows; install fzf in macos config; todo: put sketchybar config in ansible script as well

--- dot_config/sketchybar/items/calendar.lua
+++ dot_config/sketchybar/items/calendar.lua
@@ -1,39 +1,49 @@
 local settings = require("settings")
 local colors = require("colors")
 
+-- Padding item required because of bracket
+sbar.add("item", { position = "right", width = settings.group_paddings })
+
 local cal = sbar.add("item", {
 	icon = {
-		color = colors.dirty_white,
-		font = {
-			style = settings.font.style_map["Bold"],
-			size = 12.0,
-		},
-		y_offset = -1,
-		padding_right = -2,
-	},
-	label = {
-		color = colors.dirty_white,
-		width = 96,
-		align = "left",
+		color = colors.white,
+		padding_left = 8,
 		font = {
 			style = settings.font.style_map["Black"],
-			size = 14.0,
+			size = 12.0,
 		},
 	},
+	label = {
+		color = colors.white,
+		padding_right = 8,
+		width = 49,
+		align = "right",
+		font = { family = settings.font.numbers },
+	},
 	position = "right",
-	update_freq = 1,
-	y_offset = 1,
-	padding_left = -2,
+	update_freq = 30,
+	padding_left = 1,
+	padding_right = 1,
+	background = {
+		color = colors.bg2,
+		border_color = colors.black,
+		border_width = 1
+	},
+	click_script = "open -a 'Calendar'"
 })
 
-cal:subscribe({ "forced", "routine", "system_woke" }, function(env)
-	-- see (https://www.lua.org/pil/22.1.html) for lua os date format string codes
-	cal:set({
-		icon = os.date(" %a. %d %b. %Y  |"),
-		label = os.date("  %I:%M:%S %p"),
-	})
-end)
+-- Double border for calendar using a single item bracket
+sbar.add("bracket", { cal.name }, {
+	background = {
+		color = colors.transparent,
+		height = 30,
+		border_color = colors.grey,
+	}
+})
 
-cal:subscribe("mouse.clicked", function(env)
-	sbar.exec("open -a 'Dato'")
+-- Padding item required because of bracket
+sbar.add("item", { position = "right", width = settings.group_paddings })
+
+cal:subscribe({ "forced", "routine", "system_woke" }, function(env)
+	cal:set({ icon = os.date("%a. %d %b. %I :"), label = os.date("%M %p") })
 end)
