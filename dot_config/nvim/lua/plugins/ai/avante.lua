0864211534a41a92123387db4e9f5355c652853c
Isaiah Anyimi <41877924+ianyimi@users.noreply.github.com>
2025-08-14 15:07:10 -0700

    chore: minor macos install updates. updated some available tmux sessions

--- dot_config/nvim/lua/plugins/ai/avante.lua
+++ dot_config/nvim/lua/plugins/ai/avante.lua
@@ -3,7 +3,7 @@ return {
 	event = "VeryLazy",
 	lazy = false,
 	version = false, -- set this if you want to always pull the latest change
-	enabled = true,
+	enabled = false,
 
 	-- OLLAMA DEEPSEEK R1:32B (BROKEN)
 	-- opts = {
